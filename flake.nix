236e1bb4fc43093b059b8f5105689276c4d77f28
idiig <avionplat@hotmail.com>
2026-04-20 21:23:43 +0900

    init

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "kokin-kugire — draft writing env";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = with pkgs; [
+            librsvg
+            imagemagick
+            pdf2svg
+            inkscape
+            typst
+          ];
+      });
+}
